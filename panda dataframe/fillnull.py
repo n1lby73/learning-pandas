bedaa02dcd3f175713c8b2ed7f49f5828cfd34ef
n1lby73 <n1lby73@gmail.com>
2023-07-07 14:27:34 +0100

    filling null value

---
+++ panda dataframe/fillnull.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+values = {
+            'eqpkl': [24, 1, np.nan, 17, 88, 56, 75, 37, 61, 72],
+            'irmei': [50, 45, 95, 35, 40, 41, 6, 5, 100, 91],
+            'qayzr': [89, 90, 14, 39, 77, 57, np.nan, 81, 15, 49],
+            'kfpry': [92, 17, np.nan, 73, 22, 62, 67, 36, 67, 15],
+            'pqbis': [48, 63, 60, 10, 50, 43, 94, 80, 8, 90]
+        }
+
+heading = ['odlcygipnu', 'vjwdnhkabs', 'emvysnflpj', 'qrzlepvksa', 'ofzkbgthxr', 'vyxhlfmiqz', 'bfmreolcjk', 'wmtgaeyvfl', 'dbrplnoezy', 'zfjodvltpu']
+
+data = pd.DataFrame(values, index=heading)
+
+print (data)
+
+print ("\n---------------------------fill a single column with null value using user input---------------------------\n")
+
+user1 = input("what column with NaN do you want to fill: ")
+user2 = input("what value do you want to fill for null: ")
+
+print (data[user1].fillna(user2))
+
+print ("\n---------------------------fill null value with user input---------------------------\n")
+
+user3 = input("what value do you want to fill for null: ")
+
+print (data.fillna(user3))
